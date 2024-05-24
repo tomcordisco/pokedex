4a1325a4455dc48a0740e1135deaa2ccc89483a7
Tom <t.cordisco@waia.ar>
2024-05-24 17:15:34 -0300

    [ADD] SEED

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { url } from 'inspector';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+  
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const pokemonToInsert: { name: string, no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no = +segments[ segments.length - 2];
+
+      pokemonToInsert.push({ name, no });
+  
+    });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'Seed executed';
+  }
+
+}
